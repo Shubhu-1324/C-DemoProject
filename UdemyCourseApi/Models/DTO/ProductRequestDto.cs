31d49c1c66ee7262c7f562bbd012cfec31a67f0f
Shubhu-1324 <135244427+Shubhu-1324@users.noreply.github.com>
2024-12-01 00:21:12 +0530

    Crated Extension folder And wrap the JWT Object

--- UdemyCourseApi/Models/DTO/ProductRequestDto.cs
+++ UdemyCourseApi/Models/DTO/ProductRequestDto.cs
@@ -1,11 +1,25 @@
-﻿namespace UdemyCourseApi.Models.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UdemyCourseApi.Models.DTO
 {
     public class ProductRequestDto
     {
 
+
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(100, ErrorMessage = "Name cannot be longer than 100 characters.")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(500, ErrorMessage = "Name cannot be longer than 500 characters.")]
         public string Description { get; set; }
+
+        [Range(0.1, double.MaxValue, ErrorMessage = "Price must be greater than zero.")]
         public decimal Price { get; set; }
+
+
+        [Required(ErrorMessage = "Stock is required.")]
+        [Range(0, int.MaxValue, ErrorMessage = "Stock must be a non-negative number.")]
         public int Stock { get; set; }
 
         public IFormFile Image { get; set; } 
